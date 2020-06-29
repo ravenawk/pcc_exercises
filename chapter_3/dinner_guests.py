a488c6a8622576869179da36b60f2f4446fbf378
William Martin <wpmartin@gmail.com>
2020-06-29 07:22:35 -0700

    Finished up chapter 3

---
+++ chapter_3/dinner_guests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+list_of_guests = ['Arthur Martin Sr.', 'Janet Huertas', 'Grandma Martin']
+
+print(f"I have found a bigger dinner table")
+
+print(f"{list_of_guests[0]}, please join us for dinner tonight.")
+print(f"{list_of_guests[1]}, please join us for dinner tonight.") 
+print(f"{list_of_guests[2]}, please join us for dinner tonight.\n")
+
+list_of_guests.insert(0,'Grandpa Martin')
+list_of_guests.insert(2,'Aunt Ruth')
+list_of_guests.append('Aunt Grace')
+
+print(f"There are {len(list_of_guests)} guests invited for dinner.\n")
+print(f"{list_of_guests[0]}, please join us for dinner tonight.")
+print(f"{list_of_guests[1]}, please join us for dinner tonight.")
+print(f"{list_of_guests[2]}, please join us for dinner tonight.")
+print(f"{list_of_guests[3]}, please join us for dinner tonight.")
+print(f"{list_of_guests[4]}, please join us for dinner tonight.")
+print(f"{list_of_guests[5]}, please join us for dinner tonight.")
