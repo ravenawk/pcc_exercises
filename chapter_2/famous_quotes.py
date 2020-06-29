5602535c1a2635474633b1480ece84224fc178a3
William Martin <wpmartin@macbook-pro.home>
2020-06-28 17:16:44 -0700

    Added some more chapter 2 exercises

---
+++ chapter_2/famous_quotes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+author = "Steve Jobs"
+quote = "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."
+
+print(f"{author.title()} once said, {quote}")
---
+++ chapter_2/stripping_names.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+persons_name = "\tBruce Banner\n "
+
+print(persons_name)
+print(persons_name.rstrip())
+print(persons_name.lstrip())
+print(persons_name.strip())
