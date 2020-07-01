2c9eb3ef23b5e9498607498bd1f1766ff456d450
William Martin <wpmartin@macbook-pro.home>
2020-07-01 16:11:17 -0700

    Started chapter 4

---
+++ chapter_4/animals.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+some_animals = ['dog', 'cat', 'horse'] 
+
+for animal in some_animals:
+    print(f"The {animal} has been domesticated.")
+
+print("All of these animals have been domesticated.")
---
+++ chapter_4/pizzas.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+favorite_pizza_types = ['pepperoni', 'mushroom', 'olive']
+
+for pizza in  favorite_pizza_types:
+    print(f"I like {pizza} pizza!")
+
+print(f"I really like pizza!")        
