6866591e7e8fd453ec349d45c931bd5b2f712cc6
William Martin <wpmartin@gmail.com>
2020-07-04 22:05:33 -0700

    Added chapter 6

---
+++ chapter_6/favorite_numbers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Print out people and their favorite numbers
+'''
+
+people_fav_numbers: {
+        'john': 22,
+        'jessica': 45,
+        'jim': 68
+        'junior': 74
+        'jackson': 42
+        }
+
+for keys, values in people_fav_numbers.items():
+    print("
---
+++ chapter_6/person.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Represent a person in a dictionary
+'''
+
+person_info = {
+    'first_name': 'Janet',
+    'last_name': 'Huertas',
+    'city': 'Fontana',
+    'age' : 68
+    }
+
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['city'])
+print(person_info['age'])
