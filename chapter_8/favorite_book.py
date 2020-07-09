eaad1ab3681b7d67c3e51edec7648c8663ecd4c6
William Martin <wpmartin@gmail.com>
2020-07-08 20:03:52 -0700

    Added some chapter 8 exercises

---
+++ chapter_8/favorite_book.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Create a function that is called and prints out a book
+'''
+
+def favorite_book(title):
+    '''
+    Display the name of the book
+    '''
+    print(f"One of my favorite books is {title}.")
+
+favorite_book("The Fellowship of the Ring")
---
+++ chapter_8/message.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Create and call a function to print out a message.
+'''
+
+def display_message():
+    '''
+    Print out he function
+    '''
+    print("I am learning about functions, in this chapter.")
+
+display_message()
