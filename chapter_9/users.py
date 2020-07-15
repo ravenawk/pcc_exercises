4676b4ac04ced6abf90a1b7519f5853d818c5da8
William Martin <wpmartin@gmail.com>
2020-07-14 20:41:15 -0700

    Added some more to chapter 9

--- chapter_9/users.py
+++ chapter_9/users.py
@@ -4,21 +4,30 @@ Defining and using a user class
 """
 
 class User:
+    """Class to define a user account"""
+
     def __init__(self, first, last, age, sex):
         """Initialize the user"""
-       self.first_name = first
-       self.last_name = last
-       self.age = age
-       self.sex = sex
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+        self.sex = sex
+        self.full_name = first + " " + last
 
     def describe_user(self):
         """ Describe user """
-        print(f"{self.first_name} {self.last_name} is {self.age} years old and
-            is a {self.sex}")
+        print(f"{self.first_name} {self.last_name} is {self.age} years old and"
+            f" is a {self.sex}")
 
     def greet_user(self):
         """Greet user"""
-        print(f"Hello {self.first_name}!")
+        print(f"Hello {self.full_name}!")
 
 
-pparker = User('Peter', 
+pparker = User('Peter', 'Parker', 25, 'male')
+mjane = User('Mary', 'Jane', 25, 'female')
+srogers = User('Steve', 'Rogers', 85, 'male')
+
+pparker.describe_user()
+mjane.greet_user()
+srogers.describe_user()
