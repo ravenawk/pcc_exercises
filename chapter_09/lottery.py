e1efe3509048d93272642d434696e14ea578573b
Pat Martin <wpmartin@gmail.com>
2021-01-02 18:56:02 -0800

    Did some cleanup and linting on lottery.py

--- chapter_09/lottery.py
+++ chapter_09/lottery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-from random import randint, choice
+'''
+Script to print out random choice of numbers
+'''
+from random import choice
 
 winning_choice = []
-alphanum = ['a','b','c','d','e',1,3,4,5,6]
+alphanum = ['a', 'b', 'c', 'd', 'e', 1, 3, 4, 5, 6]
 
 for i in range(4):
     single_choice = choice(alphanum)
